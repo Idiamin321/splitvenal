7a8042199acf8c20ffb0e6c54631df1a449b04b8
Idiamin321 <149855187+Idiamin321@users.noreply.github.com>
2023-11-07 13:45:46 +0000

    Update BalancesListItem.svelte

--- src/lib/BalancesListItem.svelte
+++ src/lib/BalancesListItem.svelte
@@ -14,7 +14,7 @@
 		<PrimaryText>{payerName}</PrimaryText>
 		{#if amount !== 0.0}
 			<SecondaryText class={amount < 0 ? 'error-text' : 'success-text'}>
-				{amount < 0 ? 'owes' : 'receives'} ${absRounded(amount)}
+				{amount < 0 ? 'owes' : 'receives'} €{absRounded(amount)}
 			</SecondaryText>
 		{:else}
 			<SecondaryText>
