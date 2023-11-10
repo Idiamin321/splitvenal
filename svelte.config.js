cbb26e574170338771fb8434d1b2ac35b75debc4
Roisfaozi <roisfaozi55@gmail.com>
2023-11-10 20:52:11 +0700

    Fix Svelte config error

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
+	preprocess: preprocess(),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
