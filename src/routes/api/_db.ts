2f748b9094473facf05a5fec890df09e849427ef
Idiamin321 <149855187+Idiamin321@users.noreply.github.com>
2023-11-12 04:42:31 +0100

    Update _db.ts

--- src/routes/api/_db.ts
+++ src/routes/api/_db.ts
@@ -2,9 +2,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 const users = [
 	{
-		email: 'owner@owner.com',
+		email: 'mercy@guillaumemercyvenal.fr',
 		// ⚠️ CAUTION: Do not store a plain password like this. Use proper hashing and salting.
-		password: 'secret321'
+		password: 'maxguarana'
 	}
 ];
 
