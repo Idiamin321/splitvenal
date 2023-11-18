59c040bd6c3c1d77348d8e1865640ab9549512c4
Idiamin321 <dedeouabdoulaye@gmail.com>
2023-11-18 18:50:40 +0700

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 import { defineConfig } from 'vite';
-
 export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
-			$img: './static'
+			$img: './static',
+			'buffer-equal-constant-time': path.resolve(
+				'./node_modules/buffer-equal-constant-time/index.js:3:21'
+			),
+			cloudinary: path.resolve('./node_modules/cloudinary/lib-es5/api_client/execute_request.js')
 		}
 	}
 });
