e9d4cc8fd39306d3c75b3ac68cfee2f5e490406d
Idiamin321 <dedeouabdoulaye@gmail.com>
2023-11-18 19:02:07 +0700

    FIX: runtime build in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'path';
 import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
-			$img: './static',
-			'buffer-equal-constant-time': path.resolve(
-				'./node_modules/buffer-equal-constant-time/index.js:3:21'
-			),
-			cloudinary: path.resolve('./node_modules/cloudinary/lib-es5/api_client/execute_request.js')
+			$img: './static'
+
+			// cloudinary: path.resolve('./node_modules/cloudinary/lib-es5/api_client/execute_request.js')
 		}
 	}
 });
