f93cba2cdad7b79906098dc327dbf8499f1d42fe
Roisfaozi <roisfaozi55@gmail.com>
2023-12-29 23:04:25 +0700

    Refactore: move syncGroups to recentGroupsStorage.ts

--- src/lib/_modules/recentGroupsStorage.ts
+++ src/lib/_modules/recentGroupsStorage.ts
@@ -1,8 +1,10 @@
+import { getGroupsFromServer } from '$lib/api/groups/getGroups';
+import { addGroup } from '$lib/db/groups';
 import { MAX_RECENT_GROUPS_AMNT, RECENT_GROUPS_KEY } from './constants';
 
 export function storeRecentGroup(groupId: string, secretKey: string, groupName: string) {
-	let recentGroups: object[] = getRecentGroups();
-	let otherGroups = recentGroups.filter((value) => value.groupId !== groupId);
+	const recentGroups: object[] = getRecentGroups();
+	const otherGroups = recentGroups.filter((value) => value.groupId !== groupId);
 	otherGroups.unshift({ groupId, secretKey, groupName });
 	otherGroups.splice(MAX_RECENT_GROUPS_AMNT);
 	storeAllRecentGroups(otherGroups);
@@ -14,6 +16,32 @@ export function getRecentGroups(): object[] {
 
 function storeAllRecentGroups(recentGroups: object[]) {
 	localStorage.setItem(RECENT_GROUPS_KEY, JSON.stringify(recentGroups));
+}
+
+export async function syncGroups() {
+	const localGroups: object[] = getRecentGroups();
+	const dbGroups = await getGroupsFromServer();
+
+	localGroups.forEach((localGroup) => {
+		const existsInDB = dbGroups.some((dbGroup) => dbGroup.groupId === localGroup.groupId);
+		if (!existsInDB) {
+			addGroup(localGroup.groupId, localGroup.secretKey, localGroup.groupName);
+		}
+	});
+
+	dbGroups.forEach((dbGroup) => {
+		const existsLocally = localGroups.some((localGroup) => localGroup.groupId === dbGroup.groupId);
+		if (!existsLocally) {
+			localGroups.push({
+				groupId: dbGroup.groupId,
+				secretKey: dbGroup.secretKey,
+				groupName: dbGroup.groupName
+			});
+		}
+	});
+
+	storeAllRecentGroups(localGroups);
+	console.log('Groups synchronized successfully.');
 }
 
 // function isSameGroup(oldGroup: object, newGroupId: string, newSecretKey: string, newGroupName: string) {
--- src/lib/db/groups.ts
+++ src/lib/db/groups.ts
@@ -1,6 +1,3 @@
-import { RECENT_GROUPS_KEY } from '$lib/_modules/constants';
-import { getRecentGroups } from '$lib/_modules/recentGroupsStorage';
-import { getGroupsFromServer } from '$lib/api/groups/getGroups';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -27,30 +24,4 @@ export async function addGroup(groupId: string, secretKey: string, groupName: st
 
 export async function getGroups() {
 	return prisma.group.findMany();
-}
-
-export async function syncGroups() {
-	const localGroups: object[] = getRecentGroups();
-	const dbGroups = await getGroupsFromServer();
-
-	localGroups.forEach((localGroup) => {
-		const existsInDB = dbGroups.some((dbGroup) => dbGroup.groupId === localGroup.groupId);
-		if (!existsInDB) {
-			addGroup(localGroup.groupId, localGroup.secretKey, localGroup.groupName);
-		}
-	});
-
-	dbGroups.forEach((dbGroup) => {
-		const existsLocally = localGroups.some((localGroup) => localGroup.groupId === dbGroup.groupId);
-		if (!existsLocally) {
-			localGroups.push({
-				groupId: dbGroup.groupId,
-				secretKey: dbGroup.secretKey,
-				groupName: dbGroup.groupName
-			});
-		}
-	});
-
-	localStorage.setItem(RECENT_GROUPS_KEY, JSON.stringify(localGroups));
-	console.log('Groups synchronized successfully.');
 }
